15f1baa4e2b216999f77170dbe98d476a633a247
Emile Tal <emiletal@gmail.com>
2024-11-27 11:55:28 -0500

    Slides 1 and 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import './App.scss'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import { AccommodationResultPage } from './Pages/AccommodationResultPage/AccommodationResultPage'
 import ExperienceIdPage from './Pages/ExperienceIdPage/ExperienceIdPage'
 import Footer from './components/Footer/Footer'
 import { PackagesPage } from './Pages/PackagesPage/PackagesPage'
-import { SearchResultPage } from './Pages/SearchResultPage/SearchResultPage'
 
 function App() {
 
@@ -15,7 +15,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<PackagesPage />} />
-          <Route path='/experience' element={<SearchResultPage />} />
+          <Route path='/experience' element={<AccommodationResultPage />} />
           <Route path='/bundle/:id' element={<ExperienceIdPage />} />
           <Route path='' element=""></Route>
         </Routes>
