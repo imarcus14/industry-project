bc732f1cb62ed19945b4a0a96547a60f39fa7c8b
imarcus14 <ivanmarcus1@gmail.com>
2024-11-27 11:56:33 -0500

    Work on stay screen

--- src/Pages/ExperienceIdPage/ExperienceIdPage.jsx
+++ src/Pages/ExperienceIdPage/ExperienceIdPage.jsx
@@ -1,10 +1,30 @@
 import "./ExperienceIdPage.scss";
 
 import CheckoutCard from "../../components/CheckoutCard/CheckoutCard";
+import { Header } from "../../components/Header/Header";
+import cabin from "../../assets/whistler-cabin.png"
 
 const ExperienceIdPage = () => {
     return ( 
-        <CheckoutCard/>
+        <>
+            <Header/>
+            <h3>Forest View Suites Cascade Lodge Whistler</h3>
+            <div>
+                <img className="cabin" src={cabin}/>
+                <div className="cabin-small-container">
+                    <img className="cabin-small" src={cabin}/>
+                    <img className="cabin-small" src={cabin}/>
+                    <img className="cabin-small" src={cabin}/>
+                    <img className="cabin-small" src={cabin}/>
+                </div>
+            </div>
+            <div>
+                <div>
+                    
+                </div>
+            <CheckoutCard price="397" checkIn="12-23-2024" checkOut="12-30-2024" guests="2" taxes="0.13"/>
+            </div>
+        </>
      );
 }
  
