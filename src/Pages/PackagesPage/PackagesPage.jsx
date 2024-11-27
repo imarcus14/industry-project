15fa7ab25c4d54cdc822dd531e3a2002a68f971e
Emile Tal <emiletal@gmail.com>
2024-11-27 10:50:45 -0500

    Create search result page

--- src/Pages/PackagesPage/PackagesPage.jsx
+++ src/Pages/PackagesPage/PackagesPage.jsx
@@ -1,21 +1,21 @@
 import './PackagesPage.scss'
 
 import Cards from '../../components/Cards/Cards'
-import { Header } from '../../components/Header/Header'
+import { HeaderSearch } from '../../components/HeaderSearch/HeaderSearch'
 import stay from "../../assets/stay-image.avif"
 
 export function PackagesPage() {
     return (
         <>
-            <Header />
+            <HeaderSearch />
             <div className='card-list'>
-                <Cards src={stay} location="Calgary, AB" beds="5" cost="439" rating="4.99"/>
-                <Cards src={stay} location="Calgary, AB" beds="5" cost="439" rating="4.99"/>
-                <Cards src={stay} location="Calgary, AB" beds="5" cost="439" rating="4.99"/>
-                <Cards src={stay} location="Calgary, AB" beds="5" cost="439" rating="4.99"/>
+                <Cards src={stay} location="Calgary, AB" beds="5" cost="439" rating="4.99" />
+                <Cards src={stay} location="Calgary, AB" beds="5" cost="439" rating="4.99" />
+                <Cards src={stay} location="Calgary, AB" beds="5" cost="439" rating="4.99" />
+                <Cards src={stay} location="Calgary, AB" beds="5" cost="439" rating="4.99" />
             </div>
-            
+
         </>
-        
+
     )
 }
---
+++ src/Pages/SearchResultPage/SearchResultPage.jsx
@@ -0,0 +1,11 @@
+import './SearchResultPage.scss'
+
+import { Header } from '../../components/Header/Header'
+
+export function SearchResultPage() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
